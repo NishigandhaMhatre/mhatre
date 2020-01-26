0410def8eea3f569956e897a6da17d9a3e6ca5bb
NishigandhaMhatre <mhatrenishig@seattleu.edu>
2020-01-25 17:46:49 -0800

    Added Update method to ItemModel.

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -8,5 +8,16 @@
         // Add Unique attributes for Item
         // The Value of Item
         public int Value { get; set; } = 0;
+
+        public bool Update(ItemModel data)
+        {
+            //Update the base
+            Name = data.Name;
+            Description = data.Description;
+
+            //Update the extended
+            Value = data.Value;
+            return true;
+        }
     }
 }
