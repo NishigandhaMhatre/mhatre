01b9b00b037d46f037ceedcdd9d4299282e590ad
NishigandhaMhatre <mhatrenishig@seattleu.edu>
2020-02-09 12:14:44 -0800

    Added Update Async Method to DatabaseService.cs

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -44,5 +44,10 @@ namespace Mine.Services
         {
             return Database.Table<ItemModel>().Where(i => i.Id == id).FirstOrDefaultAsync();
         }
+
+        public Task<int> UpdateAsync(ItemModel item)
+        {
+            return Database.UpdateAsync(item);
+        }
     }
 }
